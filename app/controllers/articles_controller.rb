46577f9acb1b6a236ffb420399e416510add362b
DaniyalXprolabs <daniyal.malik@xprolabs.com>
2022-05-24 00:03:02 -0700

    Add the bootstrap to BLOG

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,49 @@
+class ArticlesController < ApplicationController
+  before_action :set_article , only: [:show,:destroy,:update,:edit]
+  def index
+    @articles=Article.all.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @article=Article.new
+  end
+
+  def create
+    @article=Article.new(article_params)
+    if @article.save
+      flash[:notice]="Article was saved successfully"
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  def edit
+   end
+
+  def update
+     if @article.update(article_params)
+      flash[:notice] = "Article was updated Successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to root_path
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title,:description)
+  end
+
+  def set_article
+    @article=Article.find(params[:id])
+  end
+end
