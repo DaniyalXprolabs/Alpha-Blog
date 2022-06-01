614fc50fd454cff159d3bee417007b2615b55210
DaniyalXprolabs <daniyal.malik@xprolabs.com>
2022-06-01 10:48:35 +0500

    Successfully Created user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   before_save { self.email=email.downcase }
   validates :username, presence: true, uniqueness: { case_sensitive: false } ,length: {minimum:3, maximum:25}
   VALID_EMAIL_REGEXP=/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: { case_sensitive: false } ,length: {minimum:3, maximum:25}, format: {with:VALID_EMAIL_REGEXP}
+  validates :email, presence: true, uniqueness: { case_sensitive: false } ,length: {minimum:3, maximum:50}, format: {with:VALID_EMAIL_REGEXP}
   has_secure_password
 end
